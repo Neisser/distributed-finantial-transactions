d04e0996d77b67beb2deeee6c80f1892cf8b6746
Neisser <villa.neisser@gmail.com>
2024-12-08 20:41:26 -0500

    feat(config): add base module and database connection

--- bank-b/src/app.module.ts
+++ bank-b/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { AccountsModule } from './accounts/accounts.module';
+import { Account } from './accounts/entities/account.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Account],
+      synchronize: true, // Disable in production
+    }),
+    AccountsModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ init.sql
@@ -0,0 +1,6 @@
+-- Create required databases
+CREATE DATABASE transactions_db;
+CREATE DATABASE bank_a_db;
+CREATE DATABASE bank_b_db;
+
+-- Optionally, you can add initial tables or data for each database here
